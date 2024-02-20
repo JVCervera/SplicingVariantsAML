1ee97892090fa3649b5be1d652aebc3b23075232
JVCervera <84067300+JVCervera@users.noreply.github.com>
2024-02-20 13:21:12 +0100

    Update BeatAML_config.sh

--- Scripts/BeatAML_config.sh
+++ Scripts/BeatAML_config.sh
@@ -5,41 +5,41 @@
 # Cohort variables:
 
 COHORT_PROJECT="BeatAML"
-INPUT_EXCEL="/home/mguaita/SplicingVariants/Data/BeatAML_cohort.xlsx"
-VCF_CSV="/home/mguaita/SplicingVariants/Data/BeatAML.AMLExomeFiles.csv"
-RNASEQ_CSV="/home/mguaita/SplicingVariants/Data/BeatAML.AMLRNASeqFiles.csv"
-RESULTS_DIR="/home/mguaita/SplicingVariants/Results/BeatAML"
+INPUT_EXCEL="/Data/BeatAML_cohort.xlsx"
+VCF_CSV="/Data/BeatAML.AMLExomeFiles.csv"
+RNASEQ_CSV="/Data/BeatAML.AMLRNASeqFiles.csv"
+RESULTS_DIR="/Results/BeatAML" #Without last "/"
 
 # Raw data directories
 
-VCF_DIR="/opt/Hematologia/SplicingVariantsAML/GDC_VCF/BeatAML"
-BAM_DIR="/opt/Hematologia/BeatAMLv36/STAR2pass_Genome" #Without last "/"
-STAR_SJCOUNTS_DIR="/opt/Hematologia/SplicingVariantsAML/GDC_SJ/BeatAML"
-VARIANTS_TO_SEARCH="/home/mguaita/SplicingVariants/Data/Potential_SAVs.tsv"
+VCF_DIR="/BeatAMLv36/VCF" #Without last "/"
+BAM_DIR="/BeatAMLv36/STAR2pass_Genome" #Without last "/"
+STAR_SJCOUNTS_DIR="/BeatAMLv36/STAR-SJCounts" #Without last "/"
+VARIANTS_TO_SEARCH="/Data/Potential_SAVs.tsv"
 
 
 # Scripts paths
 
-PYTHON_SCRIPTS_DIR="/home/mguaita/SplicingVariants/Scripts" #Without last "/"
-R_SCRIPTS_DIR="/home/mguaita/SplicingVariants/Scripts" #Without last "/"
+PYTHON_SCRIPTS_DIR="/Scripts" #Without last "/"
+R_SCRIPTS_DIR="/Scripts" #Without last "/"
 RNAMUT_EXECUTABLE_DIR="/path/to/rnamut" #Without last "/"
 GENE_PANEL="/path/to/rnamut/custom_GenePanel.ind"
 
 
 # Wxs vcf processing variables
 
-MUTMERGED_OUT="/home/mguaita/SplicingVariants/Results/BeatAML/BeatAML.mut_merged.txt"
-MUTMERGED_ANNOT="/home/mguaita/SplicingVariants/Results/BeatAML/BeatAML.mut_merged.annot.txt"
-VARSEARCH_DNA_RESULTS="/home/mguaita/SplicingVariants/Results/BeatAML/BeatAML.FoundVariantsDNA.tsv"
-VARSEARCH_EXCEL_DNA_VAF_RESULT="/home/mguaita/SplicingVariants/Results/BeatAML/BeatAML.FoundVariantsDNA.vaf.xlsx"
+MUTMERGED_OUT="/Results/BeatAML/BeatAML.mut_merged.txt"
+MUTMERGED_ANNOT="/Results/BeatAML/BeatAML.mut_merged.annot.txt"
+VARSEARCH_DNA_RESULTS="/Results/BeatAML/BeatAML.FoundVariantsDNA.tsv"
+VARSEARCH_EXCEL_DNA_VAF_RESULT="/Results/BeatAML/BeatAML.FoundVariantsDNA.vaf.xlsx"
 
 # Rnaseq variant calling variables
 
-FASTQ_DIR="/path/to/fastq"
-RNAMUT_VARCALL_DIR="/home/mguaita/SplicingVariants/Results/BeatAML/RNAMut" #Without last "/"
-VARSEARCH_EXCEL_RNA_RESULT="/home/mguaita/SplicingVariants/Results/BeatAML/BeatAML.FoundVariantsRNA.xlsx"
+FASTQ_DIR="/path/to/fastq" #Without last "/"
+RNAMUT_VARCALL_DIR="/Results/BeatAML/RNAMut" #Without last "/"
+VARSEARCH_EXCEL_RNA_RESULT="/Results/BeatAML/BeatAML.FoundVariantsRNA.xlsx"
 
 # Rnaseq splice junction processing variables
 
-SJ_COLLECTION_FILE="/home/mguaita/SplicingVariants/Results/BeatAML/BeatAML.SJcollection"
-SJ_OUT_DIR="/home/mguaita/SplicingVariants/Results/BeatAML/SpliceJunction"
+SJ_COLLECTION_FILE="/Results/BeatAML/BeatAML.SJcollection"
+SJ_OUT_DIR="/Results/BeatAML/SpliceJunction"
