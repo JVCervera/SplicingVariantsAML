e1046a76642278ebd300509bfa58f39bdb16177c
HematoLaFe <84067300+JVCervera@users.noreply.github.com>
2024-03-04 17:14:27 +0100

    Add files via upload

---
+++ Figures/Sashimis/SCRIPTS/ggsashimi_IDH1_BeatAML.sh
@@ -0,0 +1,15 @@
+python3 ggsashimi.py \
+ -b /TSV/IDH1.tsv \
+ -c chr2:208245319-208248660 \
+ -o /PLOTS/IDH1_BeatAML\
+ -S minus \
+ -g gencode.v36.annotation.gtf \
+ -F png \
+ -O 3 \
+ -C 3 \
+ -A mean_j \
+ --palette palette_U2AF1.txt \
+ --shrink \
+ --base-size 15 \
+ --ann-height 2  \
+ -M 3
