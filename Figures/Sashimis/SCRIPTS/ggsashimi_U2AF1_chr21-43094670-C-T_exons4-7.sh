1499b80496696902e16ed351a1d76b90d02f6b16
HematoLaFe <84067300+JVCervera@users.noreply.github.com>
2024-03-22 13:57:37 +0100

    Add files via upload

---
+++ Figures/Sashimis/SCRIPTS/ggsashimi_U2AF1_chr21-43094670-C-T_exons4-7.sh
@@ -0,0 +1,21 @@
+python3 /media/adminiis/HematoLaFe/SAVNet_interpretacion/ggsashimi.py  \
+ -b /media/adminiis/HematoLaFe/variantes_Papaemmanuil/SplicingVariants/FIGURES/Sashimis/TSV_og/U2AF1_allOtherAML.tsv \
+ -c chr21:43094471-43095743 \
+ -o /media/adminiis/HematoLaFe/variantes_Papaemmanuil/SplicingVariants/FIGURES/Sashimis/PLOTS/U2AF1_chr21-43094670-C-T_exons4-7.v3 \
+ -S minus \
+ -g /home/adminiis/Documentos/Hg38/GencodeV36/gencode.v36.annotation.gtf \
+ -F png \
+ -O 3 \
+ -C 3 \
+ -A mean_j \
+ -j /media/adminiis/HematoLaFe/variantes_Papaemmanuil/SplicingVariants/FIGURES/Sashimis/BED/U2AF1_chr21-43094670-C-T_MTFinalExon6_exones4-7_juctions.bed \
+ --palette /media/adminiis/HematoLaFe/variantes_Papaemmanuil/Variantes_sashimi/FIGURES/SASHIMIS/palette_U2AF1.txt \
+ --shrink \
+ --fix-y-scale \
+ --base-size 15 \
+ --ann-height 3 \
+ -M 4
+ 
+
+
+
