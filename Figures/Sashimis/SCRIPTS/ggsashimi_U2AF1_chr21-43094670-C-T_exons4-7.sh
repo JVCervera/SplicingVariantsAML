c3c717047590b1c4048c8039c8dbe42358f37b5f
HematoLaFe <84067300+JVCervera@users.noreply.github.com>
2024-03-22 13:59:54 +0100

    Update

--- Figures/Sashimis/SCRIPTS/ggsashimi_U2AF1_chr21-43094670-C-T_exons4-7.sh
+++ Figures/Sashimis/SCRIPTS/ggsashimi_U2AF1_chr21-43094670-C-T_exons4-7.sh
@@ -1,15 +1,14 @@
-python3 /media/adminiis/HematoLaFe/SAVNet_interpretacion/ggsashimi.py  \
- -b /media/adminiis/HematoLaFe/variantes_Papaemmanuil/SplicingVariants/FIGURES/Sashimis/TSV_og/U2AF1_allOtherAML.tsv \
+python3 ggsashimi.py  \
+ -b /TSV/U2AF1.tsv \
  -c chr21:43094471-43095743 \
- -o /media/adminiis/HematoLaFe/variantes_Papaemmanuil/SplicingVariants/FIGURES/Sashimis/PLOTS/U2AF1_chr21-43094670-C-T_exons4-7.v3 \
+ -o /PLOTS/U2AF1_chr21-43094670-C-T_exons4-7 \
  -S minus \
- -g /home/adminiis/Documentos/Hg38/GencodeV36/gencode.v36.annotation.gtf \
+ -g gencode.v36.annotation.gtf \
  -F png \
  -O 3 \
  -C 3 \
  -A mean_j \
- -j /media/adminiis/HematoLaFe/variantes_Papaemmanuil/SplicingVariants/FIGURES/Sashimis/BED/U2AF1_chr21-43094670-C-T_MTFinalExon6_exones4-7_juctions.bed \
- --palette /media/adminiis/HematoLaFe/variantes_Papaemmanuil/Variantes_sashimi/FIGURES/SASHIMIS/palette_U2AF1.txt \
+ --palette palette_3sashimis.txt \
  --shrink \
  --fix-y-scale \
  --base-size 15 \
